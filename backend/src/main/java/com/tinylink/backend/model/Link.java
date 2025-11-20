7411152692c729b5a4315af2aefb8577809e6334
BandedlaDeepthi <167881228+BandedlaDeepthi@users.noreply.github.com>
2025-11-20 14:39:21 +0530

    initial commit

---
+++ backend/src/main/java/com/tinylink/backend/model/Link.java
@@ -0,0 +1,24 @@
+	package com.tinylink.backend.model;
+	
+	import java.time.LocalDateTime;
+	
+	import jakarta.persistence.Column;
+	import jakarta.persistence.Entity;
+	import jakarta.persistence.Id;
+	import lombok.Data;
+	
+	@Entity
+	@Data
+	public class Link {
+	
+		@Id
+		private String code;
+		
+		@Column(nullable = false)
+		private String url;
+		
+		private int clicks=0;
+		private LocalDateTime lastClicked;
+		private LocalDateTime createdAt=LocalDateTime.now();
+		
+	}
